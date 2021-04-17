c2fe5d830942dc535ba06cba119151b650935ac4
Johannes Nicolai <jonico@github.com>
2021-04-17 23:56:57 +0000

    Simulate workflow runs with nektos

--- simulate-action-workflow.sh
+++ simulate-action-workflow.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-act workflow_dispatch  -P foobar=jonico/actions-runner:latest  -e events-nektos.json -W .github/workflows/visualize-matrix-build-nektos.yml  -s REDIS_PASSWORD=$REDIS_PASSWORD
+act workflow_dispatch \
+    -P foobar=jonico/actions-runner:latest  \
+    -e events-nektos.json -W .github/workflows/visualize-matrix-build-nektos.yml  \
+    -s REDIS_PASSWORD=$REDIS_PASSWORD -b
