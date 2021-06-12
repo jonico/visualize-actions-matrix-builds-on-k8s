401da1a7e114b7b6480fb4ed924517f99af70225
Johannes Nicolai <jonico@github.com>
2021-06-12 02:00:41 +0200

    First experiments with pscale

--- create-matrix-table.sh
+++ create-matrix-table.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+pscale database create octodemo
 pscale branch create octodemo initial-matrix-table
-pscale shell octocat initial-matrix-table < "create table matrix (id int, environment text, line text); create index environment on matrix (environment(10));"
-pscale deploy-request create octocat initial-matrix-table
-pscale deploy-request diff octocat 1
- pscale deploy-request deploy octocat 1
+echo "create table matrix (id int, environment text, lines text); create index environment on matrix (environment(10)); exit;" | pscale shell octodemo initial-matrix-table
+pscale deploy-request create octodemo initial-matrix-table
+pscale deploy-request diff octodemo 1
+pscale deploy-request deploy octodemo 1
+pscale branch create octodemo add-job-to-matrix-table
+echo "alter table matrix add column job text; create index job on matrix (job(10)); exit;" | pscale shell octodemo add-job-to-matrix-table
+pscale deploy-request create octodemo add-job-to-matrix-table
+pscale deploy-request diff octodemo 2
+pscale deploy-request deploy octodemo 2
---
+++ stream-pixels-ps.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pscale connect octodemo main --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=foobar"
