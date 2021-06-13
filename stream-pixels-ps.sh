6171ca81a859da4f77bc7d1f7c7db1ff1ef951eb
Johannes Nicolai <jonico@github.com>
2021-06-13 11:18:52 +0000

    Use octodemo instead of foobar as default environment

--- stream-pixels-ps.sh
+++ stream-pixels-ps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pscale connect monalisa main --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=foobar"
+pscale connect monalisa main --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=octodemo"
