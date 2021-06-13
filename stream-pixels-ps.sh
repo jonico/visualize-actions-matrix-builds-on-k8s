57c92d1602181061e7d5a4899aef5c4707ef9d03
Johannes Nicolai <jonico@github.com>
2021-06-13 09:39:18 +0000

    use foobar instead of barfoo for environment

--- stream-pixels-ps.sh
+++ stream-pixels-ps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pscale connect monalisa main --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=barfoo"
+pscale connect monalisa main --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=foobar"
