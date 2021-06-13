9fe437f59863454f658aacb893ce690c57aa2252
Johannes Nicolai <jonico@github.com>
2021-06-13 00:16:42 +0000

    Switch two monalisa DB

--- stream-pixels-ps.sh
+++ stream-pixels-ps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pscale connect octodemo add-job-to-matrix-table --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=barfoo"
+pscale connect monalisa add-job-to-matrix-table --execute "python3 stream-pixels-ps.py --sleep-interval=250 --environment=barfoo"
