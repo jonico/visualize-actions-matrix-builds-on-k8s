10009f06e35dedb256aeac172f4e9d7c9adceae1
Johannes Nicolai <jonico@github.com>
2021-06-13 11:19:48 +0000

    Use ghcr instead of dockerhub

--- simulate-action-workflow-ps.sh
+++ simulate-action-workflow-ps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 act workflow_dispatch \
-    -P octodemo=jonico/action-runner:ps  \
+    -P octodemo=ghcr.io/jonico/actions-runner:ps  \
     -e events-nektos-ps.json -W .github/workflows/visualize-matrix-build-nektos-ps.yml  \
     -s PLANETSCALE_SERVICE_TOKEN_NAME=$PLANETSCALE_SERVICE_TOKEN_NAME \
     -s PLANETSCALE_SERVICE_TOKEN=$PLANETSCALE_SERVICE_TOKEN \
