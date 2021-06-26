4c538ca1ba44c50516c8935d788197a4a1b01abe
Johannes Nicolai <jonico@github.com>
2021-06-26 23:17:24 +0000

    Recreating monalisa DB

--- create-matrix-table.sh
+++ create-matrix-table.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-pscale database create octodemo
-pscale branch create octodemo initial-matrix-table
-echo "create table matrix (id int, environment text, lines text); create index environment on matrix (environment(10)); exit;" | pscale shell octodemo initial-matrix-table
-pscale deploy-request create octodemo initial-matrix-table
-pscale deploy-request diff octodemo 1
-pscale deploy-request deploy octodemo 1
-pscale branch create octodemo add-job-to-matrix-table
-echo "alter table matrix add column job text; create index job on matrix (job(10)); exit;" | pscale shell octodemo add-job-to-matrix-table
-pscale deploy-request create octodemo add-job-to-matrix-table
-pscale deploy-request diff octodemo 2
-pscale deploy-request deploy octodemo 2
+pscale database create monalisa
+pscale branch create monalisa initial-matrix-table
+echo "create table matrix (id bigint NOT NULL AUTO_INCREMENT, environment varchar(10) NOT NULL,  lines longtext, PRIMARY KEY (id),  KEY environment (environment) )" | pscale shell monalisa initial-matrix-table
+pscale deploy-request create monalisa initial-matrix-table
+pscale deploy-request diff monalisa 1
+pscale deploy-request deploy monalisa 1
+pscale branch create monalisa add-job-to-matrix-table
+echo "alter table matrix add column job text; create index job on matrix (job(10))" | pscale shell monalisa add-job-to-matrix-table
+pscale deploy-request create monalisa add-job-to-matrix-table
+pscale deploy-request diff monalisa 2
+./
 
 
 # Alternative DB table monalisa
